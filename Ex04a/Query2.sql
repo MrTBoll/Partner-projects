f338889e78dccc51488be02b5c89f583bd121b64
MrTBoll <55199508+MrTBoll@users.noreply.github.com>
2019-10-09 14:18:30 -0500

    Create Query2.sql

---
+++ Ex04a/Query2.sql
@@ -0,0 +1,15 @@
+/* *** SQL-INSERT-CH03-02 *** */
+INSERT INTO PROJECT VALUES('2017 Q3 Production Plan',
+'Sales and Marketing', 135.00, '10-MAY-17', '15-JUN-17');
+
+/* *** SQL-INSERT-CH03-03 *** */
+INSERT INTO PROJECT
+(ProjectName, Department, MaxHours, StartDate)
+VALUES('2017 Q4 Portfolio Analysis', 'Finance',
+140.00, '05-OCT-17');
+
+/* *** SQL-INSERT-CH03-04 *** */
+INSERT INTO PROJECT
+(Department, ProjectName, MaxHours, StartDate)
+VALUES('Finance', '2017 Q4 Portfolio Analysis',
+140.00, '05-OCT-17');
