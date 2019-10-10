4dd9fccc0569b78659750fdcfd273154423101c3
wrosenquist <55398681+wrosenquist@users.noreply.github.com>
2019-10-10 14:56:15 -0500

    Update Query28-36.sql

--- Ex04a/Query28-36.sql
+++ Ex04a/Query28-36.sql
@@ -1,20 +1,20 @@
-/* *** SQL-Query-CH03-28 *** */
+/* Query-CH03-28 by TrevorBollech and WendyRosenquist */
 SELECT COUNT(*)
 FROM PROJECT;
 
-/* *** SQL-Query-CH03-29 *** */
+/* Query-CH03-29 by TrevorBollech and WendyRosenquist */
 SELECT COUNT(*) AS NumberOfProjects
 FROM PROJECT;
 
-/* *** SQL-Query-CH03-30 *** */
+/* Query-CH03-30 by TrevorBollech and WendyRosenquist */
 SELECT COUNT(Department) AS NumberOfDepartments
 FROM PROJECT;
 
-/* *** SQL-Query-CH03-31 *** */
+/* Query-CH03-31 by TrevorBollech and WendyRosenquist */
 SELECT COUNT(DISTINCT Department) AS NumberOfDepartments
 FROM PROJECT;
 
-/* *** SQL-Query-CH03-32 *** */
+/* Query-CH03-32 by TrevorBollech and WendyRosenquist */
 SELECT SUM(MaxHours) AS TotalMaxHours,
 AVG(MaxHours) AS AverageMaxHours,
 MIN(MaxHours) AS MinimumMaxHours,
@@ -22,21 +22,21 @@ MAX(MaxHours) AS MaximumMaxHours
 FROM PROJECT
 WHERE ProjectID <= 1200;
 
-/* *** SQL-Query-CH03-33 *** */   (Doesn't work)
+/* Query-CH03-33 by TrevorBollech and WendyRosenquist */   (Doesn't work)
 SELECT ProjectName, COUNT(*)
 FROM PROJECT;
 
-/* *** SQL-Query-CH03-34 *** */   (Doesn't work)
+/* Query-CH03-34 by TrevorBollech and WendyRosenquist */   (Doesn't work)
 SELECT *
 FROM PROJECT
 WHERE MaxHours > AVG(MaxHours);
 
-/* *** SQL-Query-CH03-35 *** */
+/* Query-CH03-35 by TrevorBollech and WendyRosenquist */
 SELECT ProjectID, ProjectName, MaxHours,
 (24.50 * MaxHours) AS MaxProjectCost
 FROM PROJECT;
 
-/* *** SQL-Query-CH03-36 *** */
+/* Query-CH03-36 by TrevorBollech and WendyRosenquist */
 SELECT Department, Count(*) AS NumberOfEmployees
 FROM EMPLOYEE
 GROUP BY Department;
