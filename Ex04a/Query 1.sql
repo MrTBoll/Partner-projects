cbf9547b498bcbd2dda7af1412c93e734f19e1da
wrosenquist <55398681+wrosenquist@users.noreply.github.com>
2019-10-10 15:02:56 -0500

    Update Query 1.sql

--- Ex04a/Query 1.sql
+++ Ex04a/Query 1.sql
@@ -1,3 +1,3 @@
-/* *** SQL-INSERT-CH03-01 *** */
+/* Query-CH03-01 by TrevorBollech and WendyRosenquist */
 INSERT INTO DEPARTMENT VALUES('Administration',
 'BC-100-10', 'BLDG01-210', '360-285-8100');
