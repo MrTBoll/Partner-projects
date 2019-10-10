781800a45c76264b3df70c92034b309af24bef7d
wrosenquist <55398681+wrosenquist@users.noreply.github.com>
2019-10-10 15:02:33 -0500

    Update Query2-7.sql

--- Ex04a/Query2-7.sql
+++ Ex04a/Query2-7.sql
@@ -1,28 +1,28 @@
-/* *** SQL-INSERT-CH03-02 *** */
+/* Query-CH03-02 by TrevorBollech and WendyRosenquist */
 INSERT INTO PROJECT VALUES('2017 Q3 Production Plan',
 'Sales and Marketing', 135.00, '10-MAY-17', '15-JUN-17');
 
-/* *** SQL-INSERT-CH03-03 *** */
+/* Query-CH03-03 by TrevorBollech and WendyRosenquist */
 INSERT INTO PROJECT
 (ProjectName, Department, MaxHours, StartDate)
 VALUES('2017 Q4 Portfolio Analysis', 'Finance',
 140.00, '05-OCT-17');
 
-/* *** SQL-INSERT-CH03-04 *** */
+/* Query-CH03-04 by TrevorBollech and WendyRosenquist */
 INSERT INTO PROJECT
 (Department, ProjectName, MaxHours, StartDate)
 VALUES('Finance', '2017 Q4 Portfolio Analysis',
 140.00, '05-OCT-17');
 
-/* *** SQL-Query-CH03-05 *** */
+/* Query-CH03-05 by TrevorBollech and WendyRosenquist */
 SELECT Department
 FROM PROJECT;
 
-/* *** SQL-Query-CH03-06 *** */
+/* Query-CH03-06 by TrevorBollech and WendyRosenquist */
 SELECT DISTINCT Department
 FROM PROJECT;
 
-/* *** SQL-Query-CH03-07 *** */
+/* Query-CH03-07 by TrevorBollech and WendyRosenquist */
 SELECT *
 FROM PROJECT
 WHERE Department = 'Finance';
