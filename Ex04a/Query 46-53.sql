963bf003b55da728e7623a7c561e7320ce0cfd1d
wrosenquist <55398681+wrosenquist@users.noreply.github.com>
2019-10-10 12:59:21 -0500

    Create Query 46-53.sql

---
+++ Ex04a/Query 46-53.sql
@@ -0,0 +1,51 @@
+/* *** SQL-Query-CH03-46 *** */
+SELECT FirstName, LastName, ProjectID, HoursWorked
+FROM EMPLOYEE, ASSIGNMENT
+WHERE EMPLOYEE.EmployeeNumber = ASSIGNMENT.EmployeeNumber;
+
+/* *** SQL-Query-CH03-47 *** */
+SELECT FirstName, LastName, ProjectID, HoursWorked
+FROM EMPLOYEE, ASSIGNMENT
+WHERE EMPLOYEE.EmployeeNumber = ASSIGNMENT.
+ EmployeeNumber
+ORDER BY EMPLOYEE.EmployeeNumber, ProjectID;
+
+/* *** SQL-Query-CH03-48 *** */
+SELECT FirstName, LastName, ProjectID, HoursWorked
+FROM EMPLOYEE JOIN ASSIGNMENT
+ ON EMPLOYEE.EmployeeNumber = ASSIGNMENT.EmployeeNumber
+ORDER BY EMPLOYEE.EmployeeNumber, ProjectID;
+
+/* *** SQL-Query-CH03-49 *** */
+SELECT FirstName, LastName,
+SUM(HoursWorked) AS TotalHoursWorked
+FROM EMPLOYEE AS E JOIN ASSIGNMENT AS A
+ON E.EmployeeNumber = A.EmployeeNumber
+GROUP BY LastName, FirstName
+ORDER BY LastName, FirstName;
+
+/* *** SQL-Query-CH03-50 *** */
+SELECT FirstName, LastName, ProjectID, HoursWorked
+FROM EMPLOYEE AS E JOIN ASSIGNMENT AS A
+ON E.EmployeeNumber = A.EmployeeNumber
+WHERE HoursWorked > 50
+ORDER BY LastName, FirstName, ProjectID;
+
+/* *** SQL-Query-CH03-51 *** */
+SELECT ProjectName, FirstName, LastName, HoursWorked
+FROM EMPLOYEE AS E JOIN ASSIGNMENT AS A
+ON E.EmployeeNumber = A.EmployeeNumber
+JOIN PROJECT AS P
+ ON A.ProjectID = P.ProjectID
+ORDER BY P.ProjectID, A.EmployeeNumber;
+
+/* *** SQL-Query-CH03-52 *** */
+SELECT * FROM PROJECT;
+
+/* *** SQL-Query-CH03-53 *** */
+SELECT ProjectName, FirstName, LastName, HoursWorked
+FROM EMPLOYEE AS E JOIN ASSIGNMENT AS A
+ ON E.EmployeeNumber = A.EmployeeNumber
+ JOIN PROJECT AS P
+ ON A.ProjectID = P.ProjectID
+ORDER BY P.ProjectID, A.EmployeeNumber;
