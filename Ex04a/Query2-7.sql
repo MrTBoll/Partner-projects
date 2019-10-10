576ecd319020ed42db0312dcf350d05221b1eb55
MrTBoll <55199508+MrTBoll@users.noreply.github.com>
2019-10-10 11:41:21 -0500

    Update and rename Query2.sql to Query2-7.sql

--- Ex04a/Query2-7.sql
+++ Ex04a/Query2-7.sql
@@ -13,3 +13,16 @@ INSERT INTO PROJECT
 (Department, ProjectName, MaxHours, StartDate)
 VALUES('Finance', '2017 Q4 Portfolio Analysis',
 140.00, '05-OCT-17');
+
+/* *** SQL-Query-CH03-05 *** */
+SELECT Department
+FROM PROJECT;
+
+/* *** SQL-Query-CH03-06 *** */
+SELECT DISTINCT Department
+FROM PROJECT;
+
+/* *** SQL-Query-CH03-07 *** */
+SELECT *
+FROM PROJECT
+WHERE Department = 'Finance';
