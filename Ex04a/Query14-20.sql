9a3a579c985e7d3ac6b0b19aae9a7cf03ab67278
wrosenquist <55398681+wrosenquist@users.noreply.github.com>
2019-10-10 14:59:19 -0500

    Update Query14-20.sql

--- Ex04a/Query14-20.sql
+++ Ex04a/Query14-20.sql
@@ -1,40 +1,40 @@
-/* *** SQL-Query-CH03-14 *** */
+/* Query-CH03-14 by TrevorBollech and WendyRosenquist */
 SELECT FirstName, LastName, Department, OfficePhone
 FROM EMPLOYEE
 WHERE Department = 'Accounting'
 AND OfficePhone = '360-285-8430';
 
-/* *** SQL-Query-CH03-15 *** */
+/* Query-CH03-15 by TrevorBollech and WendyRosenquist */
 SELECT FirstName, LastName, Department, OfficePhone
 FROM EMPLOYEE
 WHERE Department = 'Accounting'
 OR OfficePhone = '360-285-8410';
 
-/* *** SQL-Query-CH03-16 *** */
+/* Query-CH03-16 by TrevorBollech and WendyRosenquist */
 SELECT FirstName, LastName, Department, OfficePhone
 FROM EMPLOYEE
 WHERE Department = 'Accounting'
 AND NOT OfficePhone = '360-285-8430';
 
-/* *** SQL-Query-CH03-17 *** */
+/* Query-CH03-17 by TrevorBollech and WendyRosenquist */
 SELECT FirstName, LastName, Department, OfficePhone
 FROM EMPLOYEE
 WHERE Department IN ('Administration', 'Finance',
 'Accounting');
 
-/* *** SQL-Query-CH03-18 *** */
+/* Query-CH03-18 by TrevorBollech and WendyRosenquist */
 SELECT FirstName, LastName, Department, OfficePhone
 FROM EMPLOYEE
 WHERE Department NOT IN ('Administration', 'Finance',
 'Accounting');
 
-/* *** SQL-Query-CH03-19 *** */
+/* Query-CH03-19 by TrevorBollech and WendyRosenquist */
 SELECT FirstName, LastName, Department, OfficePhone
 FROM EMPLOYEE
 WHERE EmployeeNumber >= 2
 AND EmployeeNumber <= 5;
 
-/* *** SQL-Query-CH03-20 *** */
+/* Query-CH03-20 by TrevorBollech and WendyRosenquist */
 SELECT FirstName, LastName, Department, OfficePhone
 FROM EMPLOYEE
 WHERE EmployeeNumber BETWEEN 2 AND 5;
