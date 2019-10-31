8a75b1eabbb5186876a433a5e3d1498a3f74aa98
Disha <55199587+sdisha083@users.noreply.github.com>
2019-10-31 08:36:06 -0500

    Update SubQuery-Disha-Wendy-Trevor.sql

--- SubQuery-Disha-Wendy-Trevor.sql
+++ SubQuery-Disha-Wendy-Trevor.sql
@@ -5,3 +5,13 @@ WHERE HoursWorked>
 (SELECT MIN(HoursWorked)
 FROM assignment)
 ORDER BY HoursWorked; 
+
+Double NOT Exist
+
+SELECT DISTINCT ProjectID
+FROM Project
+WHERE NOT EXISTS (
+SELECT * FROM assignment
+WHERE NOT EXISTS(
+SELECT * FROM Assignment
+WHERE assignmnet.ProjectID=project.ProjectID));
